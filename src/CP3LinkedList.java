8df9ab68906da44dbaf21867eba78147e64cf1c6
t00202160 <paul.cafferkey@students.ittralee.ie>
2019-09-16 10:49:57 +0100

    lab2 16/9

---
+++ src/CP3LinkedList.java
@@ -0,0 +1,95 @@
+import java.util.NoSuchElementException;
+//singly linked list with first reference only 
+
+/**
+ A linked list is a sequence of links with efficient
+ element insertion and removal. This class
+ contains a subset of the methods of the standard
+ java.util.LinkedList class.
+ */
+
+public class CP3LinkedList<E> {
+    private class Node {
+        public E data;
+        public Node next;
+        public Node last;
+    }
+
+    private Node first;
+    private Node last;
+
+    /**
+     * Constructs an empty linked list.
+     */
+    public CP3LinkedList() {
+        first = null;
+        last = null;
+    }
+
+    /**
+     * Adds an element to the front of the linked list.
+     *
+     * @param element the element to add
+     */
+    public void addFirst(E element) {
+        Node newLink = new Node();
+        newLink.data = element;
+        newLink.next = first;
+        first = newLink;
+    }
+
+    public void addLast(E element) {
+//        Node newLink = new Node();
+//        newLink.data = element;
+//        newLink.last = last;
+//        last = newLink;
+        //tbf
+    }
+
+    public E getFirst() {
+        if (first == null)
+            throw new NoSuchElementException();
+        return first.data;
+    }
+
+    public E removeFirst() {
+        if (first == null)
+            throw new NoSuchElementException();
+        E element = first.data;
+        first = first.next;
+        return element;
+    }
+
+    public boolean isEmpty() {
+        /* or return first == null */
+
+        if (first == null)
+            return true;
+        else
+            return false;
+    }
+
+    public void print(){
+        //output contents of the list
+        Node current = first;
+
+        while(current != null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+
+    public int size (){
+        int size = 0;
+
+        Node current = first;
+
+        while(current != null){
+            size ++;
+            current = current.next;
+        }
+
+
+        return size;
+    }
+}
